b88b293f2c1fc5bb383302d6aede585d6ad2e35f
akmaldira <akmaldiraa@gmail.com>
2022-12-03 22:56:08 +0700

    Adding path root dir

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const db = require('./models');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
